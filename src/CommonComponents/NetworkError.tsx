8d519077c7d993119e47fe6bc6ec97e2d8a0caac
reeteshin <reeteshmeena@gmail.com>
2023-07-19 15:26:15 +0530

    Style

--- src/CommonComponents/NetworkError.tsx
+++ src/CommonComponents/NetworkError.tsx
@@ -6,16 +6,31 @@ const NetworkError = () => {
   const path = '../assets/NetworkError.json';
   return (
     <>
-    <View
-      style={{height: "50%",width:"70%", marginTop: '40%', alignSelf: 'center',marginBottom:40,flexDirection:'column-reverse'}}>
-      <AnimatedLottieView source={require(path)} autoPlay loop />
-
-    </View>
-      <Text style={{color:'darkblue',fontWeight:'bold',textAlign:'center',marginTop:20}}>Please Turn On The Internet Conection</Text>
-        </>
+      <View style={styles.lottieViewStyle}>
+        <AnimatedLottieView source={require(path)} autoPlay loop />
+      </View>
+      <Text style={styles.textStyle}>
+        Please Turn On The Internet Conection
+      </Text>
+    </>
   );
 };
 
 export default NetworkError;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  lottieViewStyle: {
+    height: '50%',
+    width: '70%',
+    marginTop: '40%',
+    alignSelf: 'center',
+    marginBottom: 40,
+    flexDirection: 'column-reverse',
+  },
+  textStyle: {
+    color: 'darkblue',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
