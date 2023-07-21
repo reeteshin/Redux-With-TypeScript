1b9cc1b020082a2e0989f22144ae8e5c4a1aed2a
reeteshin <reeteshmeena@gmail.com>
2023-07-21 14:30:22 +0530

    Reload if item is Deleted

--- src/Components/UserList.tsx
+++ src/Components/UserList.tsx
@@ -17,45 +17,46 @@ import NetworkError from '../CommonComponents/NetworkError';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../StackNavigation/StackNavigation';
 import {useNetInfo} from '@react-native-community/netinfo';
-import {SUCCESS} from '../Redux/ActionType';
 import {Success} from '../Redux/Action';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const UserList = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
   //const navigation = useNavigation()
   const {Loading, User, Error} = useSelector(
     (Value: RootState) => Value.apireducer,
   );
-  console.log('Orignal User', User);
+  console.log('Orignal User', User.data?.length);
   const dispatch = useDispatch<AppDispatch>();
- 
   //console.log('APi Call', Error);
+  //Checks if all user deleted, reload the user from api Again
+ // setReload(Check())
+  //console.log('Checks', Check());
   //type NAL = NativeStackScreenProps<RootStackParamList, 'UserDetails'>;
   //console.log('Data', User);
-  const InternetInfo =  useNetInfo()
-  console.log("Info",InternetInfo.isConnected)
+  const InternetInfo = useNetInfo();
+  console.log('Info', InternetInfo.isConnected);
 
-    useEffect(()=>{
-      dispatch(ApiMiddlware());
-    },[InternetInfo.isConnected]) //true
+  useEffect(() => {
+    dispatch(ApiMiddlware());
+  }, [(InternetInfo.isConnected) || (User.data?.length===0)   ]); 
 
   useEffect(() => {
     dispatch(ApiMiddlware());
   }, []);
   const handelAlert = (item: any) => {
-    Alert.alert('Hi', 'Do you want to delete this User ?', [
+    Alert.alert('Warn !!!', 'Do you want to delete this User ?', [
       {
         text: 'Cancel',
-        onPress: () => console.log("Nothing Will show"),
+        onPress: () => console.log('Nothing Will show'),
         style: 'cancel',
+      
       },
       {text: 'OK', onPress: () => handelDelete(item)},
     ]);
   };
 
   const handelDelete = (item: any) => {
-   
-    if (item.id ) {
+    if (item.id) {
       const filterItem = User.data.filter(
         (itemOfUser: any) => itemOfUser.id !== item.id,
       );
@@ -83,13 +84,15 @@ const UserList = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
                     onPress={() =>
                       navigation.navigate('UserDetails', {SingslUser: item})
                     }>
-                    <View style={{flexDirection:'row'}}>
+                    <View style={{flexDirection: 'row'}}>
                       <View style={styles.mappedNameAndEmail}>
                         <Text style={{color: 'black'}}>{item.name}</Text>
                         <Text style={{color: 'black'}}>{item.email}</Text>
                       </View>
-                      <TouchableOpacity style={{marginTop:40,marginRight:20}}  onPress={() => handelAlert(item)}>
-                        <Icon name='delete' size={24} color={"black"}/>
+                      <TouchableOpacity
+                        style={{marginTop: 40, marginRight: 20}}
+                        onPress={() => handelAlert(item)}>
+                        <Icon name="delete" size={24} color={'black'} />
                       </TouchableOpacity>
                     </View>
                   </TouchableOpacity>
